4c53cbc7b1c2e5e268a1922a1e5b4627146d94da
marckes04 <maresp95@gmail.com>
2022-09-02 01:29:12 -0500

    first commit

---
+++ Airplane/Assets/Plane Fighter Assets/Plane Assets/Scripts/Player Scripts/Propellor.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Propellor : MonoBehaviour
+{
+    private bool rotating = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotating)
+        {
+            transform.Rotate(Vector3.right * 40);
+        }
+    }
+
+    public void PauseRotation()
+    {
+        rotating = false;
+    }
+
+    public void ResumeRotation()
+    {
+        rotating = true;
+    }
+}
